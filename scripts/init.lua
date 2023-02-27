ab0d978e3965dfc7ad1d088f2da85509bf531209
Br00ty <aribant92@gmail.com>
2023-02-27 02:00:59 -0500

    added a shop tracker

--- scripts/init.lua
+++ scripts/init.lua
@@ -25,6 +25,7 @@ ScriptHost:LoadScript("scripts/logic/logic.lua")
 
 -- Items
 Tracker:AddItems("items/items.json")
+Tracker:AddItems("items/shopspam.json")
 
 if not IS_ITEMS_ONLY then -- <--- use variant info to optimize loading
     -- Maps
